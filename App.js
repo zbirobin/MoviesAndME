889a37ff3ea9890cf3d34acd943444582e8c6430
zbirobin <zbi.robin@gmail.com>
2020-04-19 13:51:03 +0200

    Add icons to tab

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import 'react-native-gesture-handler';
 import React from 'react'
+import { Image, StyleSheet } from 'react-native';
 import Search from './Components/Search'
 import Favorites from './Components/Favorites'
 import FilmDetail from './Components/FilmDetail'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 import { Provider } from 'react-redux'
 import Store from './Store/configureStore'
+
 
 const SearchStackNavigator = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -31,11 +35,30 @@ function MyStack() {
 
 function MyTab() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          //var sourceImage =
+          if (route.name === "StackSearch"){
+            return <Image
+                    source={require('./Images/ic_search.png')}
+                    style={styles.icon} />
+          } else { //  if (route.name === 'Favorites')
+            return <Image
+                    source={require('./Images/ic_favorite.png')}
+                    style={styles.icon} />
+          }
+        }
+      })}
+      tabBarOptions={{
+          showLabel: false,
+          activeBackgroundColor: '#DDDDDD', // Couleur d'arrière-plan de l'onglet sélectionné
+          inactiveBackgroundColor: '#FFFFFF', // Couleur d'arrière-plan des onglets non sélectionnés
+        }}>
           <Tab.Screen
           name="StackSearch"
           component={MyStack}
-          options={{title:"Rechercher"}} />
+          />
           <Tab.Screen
           name="Favorites"
           component={Favorites}
@@ -43,6 +66,13 @@ function MyTab() {
     </Tab.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
+  }
+})
 
 export default function App() {
   return (
